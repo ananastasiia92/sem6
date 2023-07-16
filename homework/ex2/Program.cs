c8fac0cb7f4d976a9071b40f3571daea4dbfccb1
Anastasia <talanovanastya@gmail.com>
2023-07-16 23:57:29 +0300

    second commit

--- homework/ex2/Program.cs
+++ homework/ex2/Program.cs
@@ -6,3 +6,28 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
 
+Console.Clear();
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+
+double x = (b2 - b1)/(k1 - k2);
+double y = (k1*(b2-b1))/(k1-k2)+b1;
+if ((k1 == k2) && (b1 == b2)){
+    Console.WriteLine("Прямые не пеересекаются");
+}else
+    if (k1 == k2){
+        Console.WriteLine("Прямые параллельны");
+    }
+    
+
+Console.WriteLine($"{x};{y}");       
